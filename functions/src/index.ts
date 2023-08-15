ba708a04ff9a5a11c90e29cd4c364eda68f96a58
sherlockian4869 <k.yaeo@carep-tech.com>
2023-08-15 23:44:49 +0900

    ユーザ作成時のトリガー

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let serviceAccount = require('../serviceAccountKey.json')
+
+// const app = express()
+
+/** Firebaseの初期化 */
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+/** Authentication処理 */
+const usersColRef = admin.firestore().collection('users')
+
+/** Authトリガー  */
+const registerUserTriggerFromAuth = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate(async (user) => {
+    try {
+      await usersColRef.doc(user.uid).set({
+        uid: user.uid,
+        username:
+          user.displayName || user.email?.split('@')[0] || 'ユーザ名未設定',
+        email: user.email,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+      })
+      return
+    } catch (error) {
+      console.error(error)
+      return
+    }
+  })
+
+module.exports = {
+  registerUserTriggerFromAuth,
+}
