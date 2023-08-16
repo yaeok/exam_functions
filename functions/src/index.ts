b7bf9670236fead53125932c3a3f83b40df2c41a
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-08-16 11:15:46 +0900

    問い合わせ登録後、Gmail送信の処理を実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,21 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
+let nodemailer = require('nodemailer')
 let serviceAccount = require('../serviceAccountKey.json')
-
-// const app = express()
 
 /** Firebaseの初期化 */
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 })
 
-/** Authentication処理 */
+/** エラーログのコレクションパス */
+const logColRef = admin.firestore().collection('logs')
+
+/** usersコレクションのパス */
 const usersColRef = admin.firestore().collection('users')
 
-/** Authトリガー  */
+/** Authにユーザが新規登録されたときに動作する処理  */
 const registerUserTriggerFromAuth = functions
   .region('asia-northeast1')
   .auth.user()
@@ -33,6 +35,48 @@ const registerUserTriggerFromAuth = functions
     }
   })
 
+/** Gmail送信用の設定変数 */
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'yaeok.engineer@gmail',
+    pass: 'Yaeo0822',
+  },
+})
+/** firestoreのcontactsに新規作成されたときに動作する処理 */
+const createContactTriggerFromFirestore = functions
+  .region('asia-northeast1')
+  .firestore.document('contacts/{contactId}')
+  .onCreate(async (snapshot, context) => {
+    try {
+      const mailOptions = {
+        from: 'yaeok.engineer@gmail.com',
+        to: 'k.yaeo@carep-tech.com',
+        subject: 'cloud functionsからのメール',
+        html: 'これはサンプルのメールです',
+      }
+
+      return transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return logColRef.add({
+            status: 'error',
+            message: error,
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+          })
+        }
+        return logColRef.add({
+          status: 'success',
+          message: 'メールが正常に送信されました',
+          createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        })
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+  })
+
 module.exports = {
   registerUserTriggerFromAuth,
+  createContactTriggerFromFirestore,
 }
