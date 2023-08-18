da48b79d7171bb7889368283115ad7e38f4d8f8b
rev-kohei.yaeo <yaeok@revolveok.co.jp>
2023-08-18 10:32:36 +0900

    成績定期送信バッチの実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,8 @@ import { format } from 'date-fns'
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
-import { Contact } from './models/contact.model'
+import { Contact } from './models/contacts.model'
+import { User } from './models/users.model'
 
 let nodemailer = require('nodemailer')
 let serviceAccount = require('../serviceAccountKey.json')
@@ -103,7 +104,85 @@ const createContactTriggerFromFirestore = functions
     }
   })
 
+/** 毎週土曜のAM 8:00今週の成績が送信される */
+const sendAnswerResultByEmailBatch = functions
+  .region('asia-northeast1')
+  .pubsub.schedule('0 8 * * 6')
+  .onRun(async (context) => {
+    const users = await admin.firestore().collection('users').get()
+    const userList: User[] = users.docs.map((doc) => {
+      return {
+        uid: doc.data().uid,
+        username: doc.data().username,
+        email: doc.data().email,
+      } as User
+    })
+    for (const user of userList) {
+      const results = await admin
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .collection('results')
+        .where(
+          'executedAt',
+          '>=',
+          new Date(new Date().setDate(new Date().getDate() - 7))
+        )
+        .get()
+      const result = results.docs.map((doc) => {
+        return {
+          numberOfCorrect: doc.data().numberOfCorrect,
+          numberOfInCorrect: doc.data().numberOfInCorrect,
+        }
+      })
+      const numberOfCorrect = result.reduce((acc, cur) => {
+        return acc + cur.numberOfCorrect
+      }, 0)
+      const numberOfInCorrect = result.reduce((acc, cur) => {
+        return acc + cur.numberOfInCorrect
+      }, 0)
+      let message = 'このメールは自動送信です。\n'
+      message += '下記の内容の今週の成績が送信されました。\n\n'
+      message += '今週の成績\n'
+      message += '問題回答数：' + (numberOfCorrect + numberOfInCorrect) + '\n'
+      message += '正解数：' + numberOfCorrect + '\n'
+      message += '誤答数：' + numberOfInCorrect + '\n'
+      message += '正解率：' + Math.round((numberOfCorrect / 10) * 100) + '%\n'
+
+      try {
+        const mailOptions = {
+          from: user.email,
+          to: 'yaeok.engineer@gmail.com',
+          subject: '【定期送信】今週の成績',
+          text: message,
+        }
+        await transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            return logColRef.add({
+              status: 'error',
+              message: error,
+              createdAt: admin.firestore.FieldValue.serverTimestamp(),
+            })
+          }
+          return logColRef.add({
+            status: 'success',
+            message: 'メールが正常に送信されました',
+            createdAt: admin.firestore.FieldValue.serverTimestamp(),
+          })
+        })
+      } catch (error) {
+        logColRef.add({
+          status: 'error',
+          message: error,
+          createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        })
+        return
+      }
+    }
+  })
+
 module.exports = {
   registerUserTriggerFromAuth,
   createContactTriggerFromFirestore,
+  sendAnswerResultByEmailBatch,
 }
